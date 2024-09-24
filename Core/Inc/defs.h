78b3e2d7deaf1f883820ea326ec0cf2a771202ab
bem012 <blaze@valormill.dev>
2024-09-24 09:51:09 -0500

    Push Module to GIT

---
+++ Core/Inc/defs.h
@@ -0,0 +1,41 @@
+#ifndef _DEFS_H
+#define _DEFS_H
+
+#include <stddef.h>
+//#include "serial_protocol.h"
+//-----------------------------------------------------------------------------
+//
+// Firmware Module Description
+//
+// General type definition header file.
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//
+// External Definitions
+//
+//-----------------------------------------------------------------------------
+
+#define INT8 char
+#define INT16 short
+#define UINT16 unsigned short
+#define UINT8  unsigned char
+#define INT32  int
+#define UINT32 unsigned int
+
+#define TRUE  1
+#define FALSE 0
+
+#define RESULT_OK		0	// Standard: 0 is success, 1 and higher are various error results
+#define RESULT_ERR		1	//
+
+//-----------------------------------------------------------------------------
+//
+// Interface Function Prototypes
+//
+//-----------------------------------------------------------------------------
+
+
+
+#endif
